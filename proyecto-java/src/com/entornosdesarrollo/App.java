8f2d29985bd1a2b6e3c6a993f08208794a2922d2
Raúl Medina Cañete <raulmc02@gmail.com>
2020-12-16 10:50:04 +0100

    Bug fixed

--- proyecto-java/src/com/entornosdesarrollo/App.java
+++ proyecto-java/src/com/entornosdesarrollo/App.java
@@ -4,7 +4,7 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hola mundo");
+System.out.println("Hola a los alumnos de 2º DAM");
 	}
 
 }
