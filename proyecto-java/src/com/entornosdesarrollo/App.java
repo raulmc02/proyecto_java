662af1f693392ebbdd57dbe5ff0adffb697e7d13
Raúl Medina Cañete <raulmc02@gmail.com>
2020-12-16 10:43:53 +0100

    Segundo commit

--- proyecto-java/src/com/entornosdesarrollo/App.java
+++ proyecto-java/src/com/entornosdesarrollo/App.java
@@ -1,10 +1,20 @@
 package com.entornosdesarrollo;
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hola mundo");
+
+
+}
+	public static String leerStringPorConsola() {
+		Scanner sc=new Scanner(System.in);
+		String result=sc.next();
+		sc.close();
+		return result;
 	}
 
 }
